0f45e7ac53c7fc572fd7046b8717770eba212a74
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-10-03 11:55:27 +0000

    Create netsukikko.lua

---
+++ netsukikko.lua
@@ -0,0 +1,8 @@
+require 'os'
+netsu = "/home/asakura/.local/bin/shit/netsukikko.sh"
+function remote_subtitle()
+     current_file =  mp.get_property("path");
+     os.execute(" bash "..netsu.." \""..current_file.."\" ");
+     mp.commandv("rescan_external_files", "reselect")
+end;
+mp.add_key_binding("b", "auto_load_subs", remote_subtitle)
