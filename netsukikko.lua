e55fb5fe11eddebf00bc1dec890fff6c1dd3c0c0
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-10-25 21:28:03 +0000

    Update netsukikko.lua

--- netsukikko.lua
+++ netsukikko.lua
@@ -1,7 +1,7 @@
 local utils = require('mp.utils')
 function remote_subtitle()
      current_file =  mp.get_property("path");
-     os.execute(" curl https://raw.githubusercontent.com/Ajatt-Tools/netsukikko/main/netsukikko.sh | bash -s \""..current_file.."\" \"-mpv\" && notify-send \"Subs found and downloaded\" || notify-send \"No subs for your anime on Kitsunekko\"");
+     os.execute(" curl -s https://raw.githubusercontent.com/Ajatt-Tools/netsukikko/main/netsukikko.sh | bash -s \""..current_file.."\" \"-mpv\" && notify-send \"Subs found and downloaded\" || notify-send \"No subs for your anime on Kitsunekko\"");
      mp.commandv("rescan_external_files", "reselect")
 end;
 mp.add_key_binding("Ctrl+a", "auto_load_subs", remote_subtitle)
